d43b982b8d045fd613e4541e7e42d82abbe59507
Farhan_Labib <58065054+labib2537@users.noreply.github.com>
2022-08-02 22:29:11 +0600

    Add files via upload

---
+++ 1553A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int t;
+int main()
+{
+    cin>>t;
+    long long d,n;
+    while(t--)
+    {
+        cin>>n;
+        if(n==9)
+        {
+            d=(n+1)/10;
+            cout << d << endl;
+        }
+        else{
+            d=(n+1)/10;
+            cout << d << endl;
+        }
+
+    }
+}
+
