796b1bd56b6e35e6dce22bf2f98eec12b667bb8c
Farhan_Labib <58065054+labib2537@users.noreply.github.com>
2022-08-03 12:54:37 +0600

    Add files via upload

---
+++ 1626A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+int t,n;
+int main()
+{
+    int a,b;
+    string s;
+    cin >> t;
+
+    while(t--)
+    {
+
+        cin >> s;
+        sort(s.begin(), s.end());
+        cout << s << endl;
+
+
+
+    }
+}
