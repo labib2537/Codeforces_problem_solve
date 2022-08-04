5f705e48e587bc409f545f3f3c95dbd277a97ecb
Farhan_Labib <58065054+labib2537@users.noreply.github.com>
2022-08-04 16:30:49 +0600

    Add files via upload

---
+++ 1684B.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include<iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    int t,cnt;
+
+    char change;
+    cin >> t;
+    while(t--)
+    {
+        int a,b,c,x,y,z;
+        cin >> a >> b >> c;
+        x = a+b+c;
+        y = b+c;
+        z = c;
+
+        cout << x << " " << y << " " << z << endl;
+
+
+    }
+}
