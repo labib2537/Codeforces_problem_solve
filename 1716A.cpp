70482e55e74811e56ffe10443e7022e1c0338dcf
Farhan_Labib <58065054+labib2537@users.noreply.github.com>
2022-08-05 12:34:54 +0600

    Add files via upload

---
+++ 1716A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include<iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    int t,cnt,m,i,j,k,l;
+
+    cin >> t;
+    int n;
+    while(t--)
+    {
+        cin >> n;
+        if(n==1)
+        {
+            cout << 2 << endl;
+        }
+        else
+        {
+            cout << (n+2)/3 << endl;
+        }
+
+
+    }
+}
