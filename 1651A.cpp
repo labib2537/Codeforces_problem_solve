7aa5d203962cc5c831c5badbbd1bee283113b26e
Farhan_Labib <58065054+labib2537@users.noreply.github.com>
2022-08-05 22:55:21 +0600

    Add files via upload

---
+++ 1651A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include<iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    int t,n;
+    long long a,b,c,x,y;
+
+    cin >> t;
+
+    while(t--)
+    {
+        cin >> n;
+        a=pow(2,n)-1;
+        cout<<a<<endl;
+
+    }
+
+}
+
