0a7817bb5ddb2ddb81ba8c160c5f472113b83dc0
Farhan_Labib <58065054+labib2537@users.noreply.github.com>
2022-08-06 15:44:25 +0600

    Add files via upload

---
+++ 1325A.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+//include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+ int main(){
+     int a,b,n,d,rem;
+     int t;cin>>t;
+     while(t--){
+        cin >> a;
+        cout << 1 <<  " " << a-1 <<endl;
+     }
+
+    return 0;
+}
