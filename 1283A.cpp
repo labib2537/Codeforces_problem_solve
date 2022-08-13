a0ab54b51cb2cb20ecbfdedd8973ab7b43b869ac
Farhan_Labib <58065054+labib2537@users.noreply.github.com>
2022-08-13 23:01:01 +0600

    Add files via upload

---
+++ 1283A.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t,n,k;
+    cin >> t;
+
+    while(t--)
+    {
+       int h,m;
+       cin >> h >> m;
+       int total_min = (h*60)+m;
+       cout << 1440-total_min << endl;
+
+
+
+    }
+
+
+
+
+}
