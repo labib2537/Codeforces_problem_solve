ca4374a3542876b016ce7b30e34313f9386318fb
Farhan_Labib <58065054+labib2537@users.noreply.github.com>
2022-08-17 14:18:50 +0600

    Add files via upload

---
+++ 1529A.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+//long long a[200010];
+
+
+int main()
+{
+    int t;
+    string s[100];
+    cin>>t;
+    while(t--)
+    {
+       int n;
+       cin >> n;
+       int a[n];
+       for(int i=0;i<n;i++)
+       {
+           cin >> a[i];
+       }
+       sort(a,a+n);
+       int c=0;
+       for(int i=1;i<n;i++)
+       {
+           if(a[i]>a[0])
+           {
+               c++;
+           }
+       }
+
+       cout << c << endl;
+
+
+    }
+}
