d87485208c6b589f0d3b0283ac4e66c996a87b84
Farhan_Labib <58065054+labib2537@users.noreply.github.com>
2022-09-08 13:52:54 +0600

    Add files via upload

---
+++ 1250F.cpp
@@ -0,0 +1,123 @@
+
+//
+//
+//
+//
+//        **********************
+//        **********************                   *
+//        **********************
+//        *****
+//        *****
+//        *****************
+//        *****************     **            ***********      *****     ****    ****           **             *********       *****
+//        *****************    ****           ************     *****     ****    ****          ****            **********      *****
+//        *****               ******          *****    ***     *****     ****    ****         ******           ***** *****     *****
+//        *****              ********         *****    ***     *****     ************        ********          *****  *****    *****
+//        *****             **********        ***********      *****     ************       **********         *****   *****   *****
+//        *****            ************       **********       *****     ************      ************        *****    *****  *****
+//        *****           *****    *****      *****  ****      *****     ****    ****     *****    *****       *****     ***** *****
+//        *****          *****      *****     *****   ****     *****     ****    ****    *****      *****      *****      **********
+//        *****         *****        *****    *****    ****    *****     ****    ****   *****        *****     *****       *********
+//
+//
+//
+
+
+
+/*
+
+ flag condition **********
+if(flag==1)
+        {
+            cout << "NO" << endl;
+        }
+        else
+        {
+            cout << "YES" <<endl;
+        }
+*/
+
+
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+//long long a[200010];
+
+//int a[100000], b[100000];
+const int N = 1e5+3;
+typedef long long ll;
+void bitwise_op()
+{
+    // a = 5(00000101), b = 9(00001001)
+    int a = 5, b = 9;
+
+    // The result is 00000001
+    cout<<"a = " << a <<","<< " b = " << b <<endl;
+    cout << "a & b = " << (a & b) << endl;
+
+    // The result is 00001101
+    cout << "a | b = " << (a | b) << endl;
+
+    // The result is 00001100
+    cout << "a ^ b = " << (a ^ b) << endl;
+
+    // The result is 11111010
+    cout << "~a = " << (~a) << endl;
+
+    // The result is 00010010
+    cout<<"b << 1" <<" = "<< (b << 1) <<endl;
+
+    // The result is 00000100
+    cout<<"b >> 1 "<<"= " << (b >> 1 )<<endl;
+
+}
+
+void reverseStr(string& str)
+{
+    int n = str.length();
+
+    // Swap character starting from two
+    // corners
+    for (int i = 0; i < n / 2; i++)
+        swap(str[i], str[n - i - 1]);
+}
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int cmp(int a, int b)
+{
+    return a>b;
+}
+
+int main()
+{
+   int n;
+    cin >> n;
+    ll a,b,r,mn=INT_MAX;
+    for(ll i=1;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+        {
+            a = n/i;
+            b = n/a;
+            r = (2*a)+(2*b);
+            if(mn>r)
+            {
+                mn = r;
+            }
+        }
+    }
+    cout << mn << endl;
+
+}
+
+
+
+
