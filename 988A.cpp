2b06154b649ed5a1e8e70f8668dc47ac956113d2
Farhan_Labib <58065054+labib2537@users.noreply.github.com>
2022-09-13 14:32:57 +0600

    Add files via upload

---
+++ 988A.cpp
@@ -0,0 +1,118 @@
+
+//
+//
+//
+//
+//        **********************
+//        **********************                   *
+//        **********************
+//        *****
+//        *****
+//        *****************
+//        *****************     **            ***********      *****     ****    ****           **             *********       *****
+//        *****************    ****           ************     *****     ****    ****          ****            **********      *****
+//        *****               ******          *****    ***     *****     ****    ****         ******           ***** *****     *****
+//        *****              ********         *****    ***     *****     ************        ********          *****  *****    *****
+//        *****             **********        ***********      *****     ************       **********         *****   *****   *****
+//        *****            ************       **********       *****     ************      ************        *****    *****  *****
+//        *****           *****    *****      *****  ****      *****     ****    ****     *****    *****       *****     ***** *****
+//        *****          *****      *****     *****   ****     *****     ****    ****    *****      *****      *****      **********
+//        *****         *****        *****    *****    ****    *****     ****    ****   *****        *****     *****       *********
+//
+//
+//
+
+// management >>> shift+f2
+// logs >> F2
+
+/*
+
+ flag condition **********
+if(flag==1)
+        {
+            cout << "NO" << endl;
+        }
+        else
+        {
+            cout << "YES" <<endl;
+        }
+*/
+
+
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+//long long a[200010];
+
+//int a[100000], b[100000];
+const int N = 1e5+3;
+typedef long long ll;
+void bitwise_op()
+{
+    // a = 5(00000101), b = 9(00001001)
+    int a = 5, b = 9;
+
+    // The result is 00000001
+    cout<<"a = " << a <<","<< " b = " << b <<endl;
+    cout << "a & b = " << (a & b) << endl;
+
+    // The result is 00001101
+    cout << "a | b = " << (a | b) << endl;
+
+    // The result is 00001100
+    cout << "a ^ b = " << (a ^ b) << endl;
+
+    // The result is 11111010
+    cout << "~a = " << (~a) << endl;
+
+    // The result is 00010010
+    cout<<"b << 1" <<" = "<< (b << 1) <<endl;
+
+    // The result is 00000100
+    cout<<"b >> 1 "<<"= " << (b >> 1 )<<endl;
+
+}
+
+void reverseStr(string& str)
+{
+    int n = str.length();
+
+    // Swap character starting from two
+    // corners
+    for (int i = 0; i < n / 2; i++)
+        swap(str[i], str[n - i - 1]);
+}
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int cmp(int a, int b)
+{
+    return a>b;
+}
+
+int vis[101];
+int main() {
+	int n, k;
+	cin >> n >> k;
+	vector<int>v;
+
+	for (int i = 1; i <= n; i++) {
+		int x; cin >> x;
+
+		if (!vis[x])v.push_back(i);
+		vis[x]++;
+	}
+	if (v.size() < k)cout << "NO\n";
+	else {
+		cout << "YES\n";
+		for (int i = 0; i < k; i++)cout << v[i] << " ";
+	}
+
+}
